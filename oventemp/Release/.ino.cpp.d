2c7b12d49a648fdbf79bdb2ba1391529be406060
Anton Golubev <Anton Golubev@Shuttle.bsb>
2017-01-25 23:12:46 +0100

    initial commit

---
+++ oventemp/Release/.ino.cpp.d
@@ -0,0 +1,40 @@
+.ino.cpp.o: ../.ino.cpp \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\eightanaloginputs/pins_arduino.h \
+ C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\eightanaloginputs/../standard/pins_arduino.h \
+ ../oventemp.ino
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\eightanaloginputs/pins_arduino.h:
+
+C:\Users\Anton\ Golubev\eclipse\cpp-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\eightanaloginputs/../standard/pins_arduino.h:
+
+../oventemp.ino:
